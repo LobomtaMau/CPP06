b757fc2fc1820684be6d0de6bf1d7348ec64bb98
lobomtamau <lobomtamau@gmail.com>
2024-06-24 14:22:33 +0100

    2nd

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,12 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other) {}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {
+    return *this;
+}
+
+ScalarConverter::~ScalarConverter() {}
+
